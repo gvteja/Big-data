9ee723f22b481d8da290553352593b014ee458e8
Vijay Teja <gvteja@gmail.com>
2016-11-10 19:41:49 -0500

    Fix minor bugs

--- Contextual Word Counter on Spark/a2_gottipati.py
+++ Contextual Word Counter on Spark/a2_gottipati.py
@@ -27,7 +27,14 @@ def computeCounts(rdd):
 wet_path = 's3://commoncrawl/crawl-data/CC-MAIN-2016-40/wet.paths.gz'
 cc_prefix = 's3://commoncrawl/'
 my_bucket = 's3://vijay-bd'
+# base_dir = '/Users/bobby/Downloads'
+# wet_path = path.join(base_dir, 'tmp.txt')
+# cc_prefix = path.join(base_dir, 'crawl-data')
+# my_bucket = base_dir
 
+sc = SparkContext(appName="ContextualWordCount")
+# first5k = sc.textFile(wet_path).take(3)
+# firstk, next4k = first5k[:1], first5k[1:]
 first5k = sc.textFile(wet_path).take(5000)
 firstk, next4k = first5k[:1000], first5k[1000:]
 firstk = [path.join(cc_prefix, fn) for fn in firstk]
@@ -35,14 +42,12 @@ next4k = [path.join(cc_prefix, fn) for fn in next4k]
 firstk_combined = ','.join(firstk)
 next4k_combined = ','.join(next4k)
 
-
-sc = SparkContext(appName="ContextualWordCount")
-firstk_counts = computeCounts(sc.textFile(firstk))
+firstk_counts = computeCounts(sc.textFile(firstk_combined))
 firstk_counts.filter(lambda x: x[1] >= 30)\
     .coalesce(1)\
     .saveAsTextFile(path.join(my_bucket, 'firstk.txt'))
 
-next4k_counts = computeCounts(sc.textFile(next4k))
+next4k_counts = computeCounts(sc.textFile(next4k_combined))
 first5k_counts = firstk_counts.union(next4k_counts)\
     .reduceByKey(lambda x,y: x + y)
 first5k_counts.filter(lambda x: x[1] >= 30)\
