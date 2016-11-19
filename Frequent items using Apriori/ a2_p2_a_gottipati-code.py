04cfa1118e96e9dbb4e8047a6dd53a0cf189deeb
Vijay Teja <gvteja@gmail.com>
2016-11-19 04:36:41 -0500

    Generate transactions and candidates

--- Frequent items using Apriori/ a2_p2_a_gottipati-code.py
+++ Frequent items using Apriori/ a2_p2_a_gottipati-code.py
@@ -1,4 +1,4 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, StorageLevel
 
 sc = SparkContext(appName="Apriori_Vijay")
 
@@ -6,8 +6,32 @@ def extractItemUser(line):
     user, item, _, _ = line.split(',')
     return (item, user)
 
-rdd = sc.textFile('/Users/bobby/Downloads/ratings_Beauty.csv', use_unicode=False)
-#rdd = sc.parallelize(rdd, 100)
+def generateCandidate(tup):
+    s1, s2 = tup
+    n = len(s1)
+    union = s1.union(s2)
+    if len(union) != n + 1:
+        # cannot combine the two sets
+        return []
+    [e2] = union - s1
+    [e1] = union - s2
+    if e1 > e2:
+        # cartesian product generates duplicates
+        # i.e. (s1, s2) and (s2, s1)
+        # so skipping the pair where s1 has the bigger eleme in the union
+        return []
+    return union
+
+def isFrequent(s):
+    # return true if the item-set is valid and frequent
+    if not s:
+        return False
+
+    # todo: check frequency on transactions
+
+
+rdd = sc.textFile('hdfs:/ratings/ratings_Video_Games.10k.csv.gz', use_unicode=False)
+#rdd = sc.textFile('/Users/bobby/Downloads/ratings_Beauty.csv', use_unicode=False)
 item_user_map = rdd.map(extractItemUser).distinct()
 items_to_remove = item_user_map.map(lambda x: (x[0], 1))\
     .reduceByKey(lambda x,y: x + y)\
@@ -20,7 +44,18 @@ users_to_remove = user_item_map.map(lambda x: (x[0], 1))\
     .filter(lambda x: x[1] < 5)
 user_item_map = user_item_map.subtractByKey(users_to_remove)
 
-#item_user_map.map(lambda x: (x[0], 1)).distinct().count()
+transactions = user_item_map.map(lambda x: (x[0], [x[1]]))\
+    .reduceByKey(lambda x,y: x + y)\
+    .map(lambda x: set(x[1]))\
+    .persist(StorageLevel.MEMORY_AND_DISK)
+
+
+set1 = user_item_map.values()\
+    .distinct()\
+    .map(lambda x: {x})
+set2 = set1.cartesian(set1)\
+    .map(generateCandidate)\
+    .filter(prune)
 
 # For beauty product ratings
 # In [6]: user_item_map.count()
@@ -38,6 +73,12 @@ user_item_map = user_item_map.subtractByKey(users_to_remove)
 # In [10]: item_user_map.count()
 # Out[10]: 1514617
 
+# In [16]: user_item_map.map(lambda x: (x[0], 1)).distinct().count()
+# Out[16]: 33353
+
+# In [18]: user_item_map.map(lambda x: (x[1], 1)).distinct().count()
+# Out[18]: 33327
+
 # generate cand set from freq[k-1]
 # prune cand set by using subset property
 # generate count for each remaining cand
@@ -48,3 +89,23 @@ user_item_map = user_item_map.subtractByKey(users_to_remove)
 # cand gen
 # prune
 # count cand
+
+# one line of thought:
+# do a group by on the user_item_map. we get transactions, kind of
+# maybe map value and create a set out of it
+
+# cand gen:
+#     do self join of k-1 sets
+#     merge both sets. new set should increase by just one
+#     but we will have duplicate, 2 for each set
+#     eg: (1,2) with (2,3) and (2,3) with (1,2) => (1,2,3)
+
+# prune:
+#     filter fn on cand set which check all k-1 subsets is in old k-1 subset
+#     maybe use a all(generator for set check)
+
+# and then for count do a reduce/agg
+#     but we need to pass in a set of items to all reducers
+#         for each cand, we need a bcase for its set of elems
+#     can we use a broadcast var? can we use same var name and over-write it?
+#     no right? bcast var cannot be written to i guess
