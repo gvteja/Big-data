462e0e5feca826037d9f6adbeb74b46714bd7e5a
Vijay Teja <gvteja@gmail.com>
2016-11-26 19:41:50 -0500

    Refactor code and add results list

--- Frequent items using Apriori/test.py
+++ Frequent items using Apriori/test.py
@@ -1,104 +1,118 @@
 from pyspark import SparkContext, StorageLevel
+from os import path
+from time import time
+import pickle
 
-sc = SparkContext(appName="Apriori_test_Vijay")
-
+support = 2
+aggressive_pruning = False
+#input_file = 'test'
+input_file = 'hdfs:/ratings/ratings_Video_Games.10k.csv.gz'
+basename = path.basename(input_file)
+ts = str(time())
+results = []
+suffix = '_{0}_{1}'.format(basename, ts)
+sc = SparkContext(appName='Vj_Apriori' + suffix)
 
 def generateCandidate(tup):
-    (s1, _),  (s2, _) = tup
-    s1, s2 = set(s1), set(s2)
-    try:
-        n = len(s1)
-        union = s1.union(s2)
-    except:
-        print tup, s1, s2
-    if len(union) != n + 1:
-        # cannot combine the two sets
-        return []
-    [e2] = union - s1
-    [e1] = union - s2
-    if e1 > e2:
-        # cartesian product generates duplicates
-        # i.e. (s1, s2) and (s2, s1)
-        # so skipping the pair where s1 has the bigger element in the union
-        return []
-    return tuple(sorted(union))
-
-def generateCandidate2(tup):
     (common_items, (i1, i2)) = tup
-    if not i1 or not i2:
-        # generating from 1-set
-        common_items = [] 
     if i1 < i2:
-        new_items = [i1, i2]
-    return tuple(sorted(union))
+        combined_items = common_items + (i1, i2)
+        return [combined_items]
+    return []
 
-def generatePruneDependencies(tup):
-    # assuming the tuple here is already sorted
-    l = list(tup)
-    n = len(l)
-    dependencies = []
-    for i in range(n):
-        dep = l[:i] + l[i+1:]
-        dependencies.append((tuple(dep), tup))
+def doesContain(container, items):
+    container = set(container)
+    return all((item in container for item in items))
 
-    return dependencies
+if input_file == "test":
+    test_data = '''1,2,5
+    2,4
+    2,3
+    1,2,3,4,5
+    1,3
+    2,3
+    1,3
+    1,2,3,5
+    1,2,3'''
+    test = sc.parallelize(test_data.split('\n'))\
+        .map(lambda x: tuple(x.split(',')))
+    transactions = test
+    support = 2
 
-def isSetInTransaction(tup):
-    s, t = tup
-    t = set(t)
-    in_t = all((item in t for item in s))
-    val = 1 if in_t else 0
-    return (s, val)
+    # 1-set frequent items
+    frequent_items = test.flatMap(lambda x: [(i, 1) for i in x])\
+        .reduceByKey(lambda x,y: x + y)\
+        .filter(lambda x: x[1] >= support)
+else:
+    support = 10
 
+    def extractItemUser(line):
+        user, item, _, _ = [x.strip() for x in line.split(',')]
+        return (item, user)
 
-test_data1 = '''1,2,5
-2,4
-2,3
-1,2,4
-1,3
-2,3
-1,3
-1,2,3,5
-1,2,3'''
-test_data2 = '''1,2,5
-2,4
-2,3
-1,2,3,4,5
-1,3
-2,3
-1,3
-1,2,3,5
-1,2,3'''
-test = sc.parallelize(test_data2.split('\n'))\
-    .map(lambda x: tuple(x.split(',')))
-transactions = test
-support = 2
+    rdd = sc.textFile(input_file, use_unicode=False)
+    #rdd = sc.textFile('/Users/bobby/Downloads/ratings_Beauty.csv', use_unicode=False)
+    item_user_map = rdd.map(extractItemUser).distinct()
+    items_to_remove = item_user_map.map(lambda x: (x[0], 1))\
+        .reduceByKey(lambda x,y: x + y)\
+        .filter(lambda x: x[1] < 10)
+    item_user_map = item_user_map.subtractByKey(items_to_remove)
 
-previous_set = test.flatMap(lambda x: [(i, 1) for i in x])\
-    .reduceByKey(lambda x,y: x + y)\
-    .filter(lambda x: x[1] >= support)\
-    .map(lambda x: ((x[0],), None))
+    user_item_map = item_user_map.map(lambda x: (x[1], x[0]))
+    users_to_remove = user_item_map.map(lambda x: (x[0], 1))\
+        .reduceByKey(lambda x,y: x + y)\
+        .filter(lambda x: x[1] < 5)
+    user_item_map = user_item_map.subtractByKey(users_to_remove)
 
-desired_k = 3
+    transactions = user_item_map.map(lambda x: (x[0], [x[1]]))\
+        .reduceByKey(lambda x,y: x + y)\
+        .map(lambda x: tuple(x[1]))\
+        .persist(StorageLevel.MEMORY_AND_DISK)
+    transactions.count() # to trigger caching
+
+    # 1-set frequent items
+    frequent_items = user_item_map.map(lambda x: (x[1], 1))\
+        .reduceByKey(lambda x,y: x + y)\
+        .filter(lambda x: x[1] >= support)
+
+previous_set = frequent_items.keys()
+results.append(
+        '1-set count: {0}'.format(previous_set.count()))
+
+desired_k = 4
 current_k = 2
 while current_k <= desired_k:
-    prune_deps = previous_set.cartesian(previous_set)\
-        .map(generateCandidate)\
-        .filter(lambda x: len(x) > 0)\
-        .distinct()\
-        .flatMap(generatePruneDependencies)
-    pruned_candidates = previous_set.join(prune_deps)\
-        .map(lambda x: (x[1][1], 1))\
-        .reduceByKey(lambda x,y: x + y)\
-        .filter(lambda x: x[1] == current_k)\
-        .keys()
+    if current_k == 2:
+        # pruned_candidates are just unique pairs of
+        # 1-set frequent items
+        candidates = previous_set.cartesian(previous_set)\
+            .filter(lambda x: x[0] < x[1])
+    else:
+        previous_set = previous_set.map(lambda x: (x[:-1], x[-1]))
+        candidates = previous_set.join(previous_set)\
+            .flatMap(generateCandidate)
 
-    current_set = pruned_candidates.cartesian(transactions)\
-        .map(isSetInTransaction)\
+    if aggressive_pruning: 
+        candidates = candidates.cartesian(previous_set)\
+            .filter(lambda x: doesContain(x[0], x[1]))\
+            .map(lambda x: (x[0], 1))\
+            .reduceByKey(lambda x,y: x + y)\
+            .filter(lambda x: x[1] == current_k)\
+            .keys()
+    
+    current_set = candidates.cartesian(transactions)\
+        .filter(lambda x: doesContain(x[1], x[0]))\
+        .map(lambda x: (x[0], 1))\
         .reduceByKey(lambda x,y: x + y)\
         .filter(lambda x: x[1] >= support)\
-        .map(lambda x: (x[0], None))
+        .keys()
 
+    results.append(
+        '{0}-set count: {1}'.format(current_k, current_set.count()))
     previous_set = current_set
     current_k += 1
-    print current_set.collect()
+
+results.append(current_set.collect())
+
+pickle.dump(results, \
+    open('results' + suffix, "wb"))
